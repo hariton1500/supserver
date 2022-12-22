d5b4db293d9893c7ed05fe93bdab3776ad370fec
hariton1500 <57896518+hariton1500@users.noreply.github.com>
2022-12-22 18:21:34 +0300

    2

--- lib/supserver.dart
+++ lib/supserver.dart
@@ -6,5 +6,28 @@ int calculate() {
 }
 
 Future<String> run() async {
+  HttpServer server;
+
+  try {
+    server = await HttpServer.bind(InternetAddress.anyIPv4, 4045);
+  } catch (e) {
+    print("Couldn't bind to port 4045: $e");
+    exit(-1);
+  }
+  print('Listening on http://${server.address.address}:${server.port}/');
+
+  await for (HttpRequest req in server) {
+    print(DateTime.now());
+    req.response.headers.contentType = ContentType.json;
+    //CORS Header, so the anybody can use this
+    req.response.headers.add(
+      'Access-Control-Allow-Origin',
+      '*',
+      preserveHeaderCase: true,
+    );
+
+    print(
+        '[${req.connectionInfo?.remoteAddress.address}] -> ${req.uri.path} -> ${req.requestedUri.queryParameters}');
+  }
   return 'done.';
 }
