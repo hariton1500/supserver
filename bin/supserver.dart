7a7b2c9b60e7bd6937ddfd62a7318038a017fcad
hariton1500 <57896518+hariton1500@users.noreply.github.com>
2022-12-22 18:18:17 +0300

    1

---
+++ bin/supserver.dart
@@ -0,0 +1,5 @@
+import 'package:supserver/supserver.dart' as supserver;
+
+void main(List<String> arguments) {
+  print('Hello world: ${supserver.calculate()}!');
+}
---
+++ lib/supserver.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
+int calculate() {
+  return 6 * 7;
+}
+
+Future<String> run() async {
+  return 'done.';
+}
